09702ffc919b5b4f364f3f4970a700290b526942
TakashiTanaka <39241053+TakashiTanaka@users.noreply.github.com>
2021-08-15 20:42:15 +0900

    webpack + babelファイル追加

---
+++ js-practice/webpack-babel/src/component/test.js
@@ -0,0 +1,10 @@
+/**
+ * /src/component/test.js
+ */
+const test = ({ foo, bar, ...others }) => {
+	console.log(others);
+}
+
+export { test };
+
+
---
+++ js-practice/webpack-babel/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+	mode: 'development',
+	devtool: 'inline-source-map',
+	entry: './src/index.js',
+	output: {
+		path: __dirname + '/dist',
+		filename: 'sample.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			}
+		]
+	}
+};
