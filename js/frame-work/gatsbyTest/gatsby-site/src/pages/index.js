72e28599736747a1aafe84742fa8e1a4aa26e872
TakashiTanaka <39241053+TakashiTanaka@users.noreply.github.com>
2021-09-19 22:50:54 +0900

    gatsvyテストのファイル追加

---
+++ js/frame-work/gatsbyTest/gatsby-site/src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Here is my site!!</h1>
+      <h2>test!!</h2>
+      <p>emmetが使えるようにしてみた</p>
+    </div>
+  )
+}
