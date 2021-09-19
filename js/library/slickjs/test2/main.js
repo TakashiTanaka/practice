8117856265c0ff749697f3637f41c77c956aa5cd
TakashiTanaka <39241053+TakashiTanaka@users.noreply.github.com>
2021-09-19 21:35:30 +0900

    slickのテスト追加

---
+++ js/library/slickjs/test2/main.js
@@ -0,0 +1,16 @@
+$(function () {
+	$('#slide').slick({
+		autoplay: true, // 自動でスライドするか
+		autoplaySpeed: 5000, // 自動スライドのスピード
+		dots: true, // ドットナビゲーションを表示するか
+		arrows: false, // 左右の矢印をつけるかどうか
+		dots: false,
+	});
+	$('#slide2').slick({
+		autoplay: true, // 自動でスライドするか
+		autoplaySpeed: 5000, // 自動スライドのスピード
+		dots: true, // ドットナビゲーションを表示するか
+		arrows: false, // 左右の矢印をつけるかどうか
+		dots: false,
+	});
+});
