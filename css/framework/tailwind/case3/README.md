eb07fb6d282a3f7ead564f26467947c10164955c
takashitanaka <takashi.tanaka.design@gmail.com>
2022-03-20 22:48:32 +0900

    テスト追加

--- css/framework/tailwind/case3/README.md
+++ css/framework/tailwind/case3/README.md
@@ -1,4 +1,11 @@
-# case2 groupを使って子要素に疑似クラスを伝搬させる
+# tailwindで手軽にAnimate.cssを使うためのanimated-tailwindcssを導入
 
-`group`を使うことで子要素に疑似クラスを伝搬させる。
+`animated-tailwindcss`を組み込むことで`Animate.css`のアニメーションが簡単に利用できる
 
+組み込みのアニメーションは上書きされてしまうので、注意が必要
+
+[サンプルはここで見れる](https://ikcb.org/animated-tailwindcss/docs/animations)
+
+## 参考
+
+[tw\-Animated](https://ikcb.org/animated-tailwindcss)
--- css/framework/tailwind/case3/tailwind.config.js
+++ css/framework/tailwind/case3/tailwind.config.js
@@ -1,9 +1,19 @@
 const withAnimations = require('animated-tailwindcss');
 
 module.exports = withAnimations({
-  content: ['./dist/**/*.{html,js}'],
+  content: ['./dist/**/*.{html,js}', './dist/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
+    },
   },
   plugins: [],
 });
