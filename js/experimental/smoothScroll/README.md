5a03020f4709ab9f91e4059bd944cc93df3b575e
takashitanaka <takashi.tanaka.design@gmail.com>
2022-03-20 17:55:00 +0900

    gsapのテスト追加

--- js/experimental/smoothScroll/README.md
+++ js/experimental/smoothScroll/README.md
@@ -6,9 +6,12 @@
 - [Case4 JSライブラリ、Smooth Scrollでスクロール](./case4/dist/)
 - [Case5 JSライブラリ、Smooth Scrollでスクロール（headerがstickyの時）](./case5/dist/)
 - [Case6 JSライブラリ、Smooth Scrollでスクロール（headerがstickyの時2）](./case6/dist/)
+- [Case7 JSライブラリ、GSAPでスクロール](./case7/dist/)
 
 ## 参考
 
 [今どきのスムーズスクロール(2019年版) - to-R Media](https://www.to-r.net/media/smooth_scrolling_2019/)
 
 [cferdinandi/smooth\-scroll: A lightweight script to animate scrolling to anchor links\.](https://github.com/cferdinandi/smooth-scroll)
+
+[GSAP \- GreenSock](https://greensock.com/gsap/)
---
+++ js/experimental/smoothScroll/case7/src/main.js
@@ -0,0 +1,23 @@
+// gsapを読み込み
+import { gsap } from 'gsap';
+
+// ScrollToPluginを読み込み
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
+
+// ScrollToPluginを登録
+gsap.registerPlugin(ScrollToPlugin);
+
+// hrefの最初に#が記述されているaタグすべてを処理
+document.querySelectorAll('a[href*="#"]').forEach(a => {
+  // クリックイベントを設定
+  a.addEventListener('click', e => {
+    // デフォルトの処理を無効化
+    e.preventDefault();
+    /** @type {number} - スクロール先のハッシュ */
+    const hash = e.target.hash;
+    /** @type {number} - ヘッダーの高さ */
+    const headerHeight = document.querySelector('#header').getBoundingClientRect().height;
+    // スクロール処理
+    gsap.to(window, { duration: 2, scrollTo: { y: hash, offsetY: headerHeight }, ease: 'bounce' });
+  });
+});
