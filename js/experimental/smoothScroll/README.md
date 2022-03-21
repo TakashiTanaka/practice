d125abdd8483c745708c3b28ae5e5ff9d06119bc
takashitanaka <takashi.tanaka.design@gmail.com>
2022-03-21 15:52:31 +0900

    jqueryのテスト追加

--- js/experimental/smoothScroll/README.md
+++ js/experimental/smoothScroll/README.md
@@ -2,11 +2,12 @@
 
 - [Case1 CSSでhtml要素にscroll-behavior: smooth;を適用](./case1/dist/)
 - [Case2 scrollIntoViewでスクロール](./case2/dist/)
-- [Case3 window.scrollでスクロール](./case3/dist/)
+- [Case3 scrollToでスクロール](./case3/dist/)
 - [Case4 JSライブラリ、Smooth Scrollでスクロール](./case4/dist/)
 - [Case5 JSライブラリ、Smooth Scrollでスクロール（headerがstickyの時）](./case5/dist/)
 - [Case6 JSライブラリ、Smooth Scrollでスクロール（headerがstickyの時2）](./case6/dist/)
 - [Case7 JSライブラリ、GSAPでスクロール](./case7/dist/)
+- [Case8 JSライブラリ、jQueryでスクロール](./case8/dist/)
 
 ## 参考
 
---
+++ js/experimental/smoothScroll/case8/src/main.js
@@ -0,0 +1,25 @@
+// jQueryを読み込み
+import $ from 'jquery';
+
+// jQuery用のイージング関数を読み込み
+import 'jquery.easing';
+
+// クリックイベントを設定
+$('a[href*="#"]').click(e => {
+  // デフォルトの処理を無効化
+  e.preventDefault();
+  /** @type {string} - スクロール先のハッシュ */
+  const hash = e.target.hash;
+  /** @type {JQuery<HTMLElement>} - スクロール先の要素 */
+  const targetEl = $(hash);
+  /** @type {number} - スクロール先の要素のY値 */
+  let targetElPosition = targetEl.offset().top;
+  /** @type {number} - ヘッダーの高さ */
+  const headerHeight = $('#header').height();
+  /** @type {number} - スクロール先のY値（） */
+  const position = targetElPosition - headerHeight;
+  /** @type {number} - スクロールのスピード */
+  let speed = 1000;
+  // スクロールの実行
+  $('html,body').animate({ scrollTop: position }, speed, 'easeInOutCirc');
+});
