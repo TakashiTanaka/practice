1cd8b470f2b809c4fa966d4b5d0f8ebcad43025c
TakashiTanaka <39241053+TakashiTanaka@users.noreply.github.com>
2022-05-06 18:36:18 +0900

    stringのメソッドテスト追加

---
+++ js/experimental/string-includes/main.js
@@ -0,0 +1,9 @@
+const str1 = 'The quick brown fox jumps over the lazy dog.';
+const str2 = 'fox';
+const str3 = 'red';
+
+console.log(str1.includes(str2));
+// true
+
+console.log(str1.includes(str3));
+// false
---
+++ js/experimental/string-trim/main.js
@@ -0,0 +1,3 @@
+const str = ' hey! ';
+console.log(str.trim());
+// hey!
