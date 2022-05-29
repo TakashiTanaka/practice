c68cbe17c6a2676eeac7bd6a5b0c09104450f9cd
TakashiTanaka <39241053+TakashiTanaka@users.noreply.github.com>
2022-05-29 12:56:19 +0900

    indexOfのテスト追加

---
+++ js/experimental/indexOf/main.js
@@ -0,0 +1,12 @@
+const pets = ['dog', 'cat', 'bat'];
+
+console.log(pets.indexOf('cat'));
+// 1
+
+const str = "You can't take it with you";
+
+console.log(str.indexOf('take'));
+// 10
+
+console.log(str.indexOf('it'));
+// 15
