41086bc822c920a210c4035d4c6b127aaae604e4
TakashiTanaka <39241053+TakashiTanaka@users.noreply.github.com>
2022-07-09 14:02:52 +0900

    グラフィックプログラミングの本の練習追加

---
+++ html/canvas2d/graphics-programming-intro/4-2/1-fillrect/main.js
@@ -0,0 +1,10 @@
+let canvas = document.querySelector('#main_canvas');
+
+/**
+ * @type {CanvasRenderingContext2D}
+ */
+let context = canvas.getContext('2d');
+
+context.fillRect(0, 0, 100, 100);
+// context.fillStyle = '#ffffff';
+// context.strokeStyle = '#ff0000';
